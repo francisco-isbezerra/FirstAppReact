b4f9c487d30d591d9d5800ceafe0e75346315dba
FRANCISCO IRANILDO DA SILVA BEZERRA <francisco.isbezerra@senacsp.edu.br>
2026-09-08 22:07:26 -0300

    feat: add react vs js tab

--- src/App.jsx
+++ src/App.jsx
@@ -68,6 +68,12 @@ export default function App() {
           >
             Related Resources
           </button>
+          <button
+            className={activeContentIndex === 3 ? "active" : ""}
+            onClick={() => setActiveContentIndex(3)}
+          >
+            React vs JS
+          </button>
         </menu>
 
         <div id="tab-content">
