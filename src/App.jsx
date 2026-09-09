27086072357e14e9efa017d480f291536b3b6d2c
FRANCISCO IRANILDO DA SILVA BEZERRA <francisco.isbezerra@senacsp.edu.br>
2026-09-08 21:30:07 -0300

    refactor: move tab content to data array

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,35 @@
 import ReactLogo from './assets/logo.png'
 import './styles.css'
-// const content = [
 
-// ]
-
+// content[0] → Why React?
+// content[1] → Core Features
+// content[2] → Related Resources
+// content[3] → React vs JS
+const content = [
+  [
+    "React is extremely popular",
+    "It makes building complex, interactive UIs a breeze",
+    "It's powerful & flexible",
+    "It has a very active and versatile ecosystem"
+  ],
+  [
+    "Components, JSX & Props",
+    "State",
+    "Hooks (e.g., useEffect())",
+    "Dynamic rendering"
+  ],
+  [
+    "Official web page (react.dev)",
+    "Next.js (Fullstack framework)",
+    "React Native (build native mobile apps with React)"
+  ],
+  [
+    "Vanilla JavaScript requires imperative programming",
+    "Imperative Programming: You define all the steps needed to achieve a result",
+    "React on the other hand embraces declarative programming",
+    "With React, you define the goal and React figures out how to get there"
+  ]
+];
 
 export default function App() {
   return (
